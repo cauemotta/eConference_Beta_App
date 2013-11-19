f9de230a16e50902929e04dd01bf0e93a916cc8d
Caue Motta <caue_motta@hotmail.com>
2013-11-20 01:30:54 +1100

    Tried wrapping methods
    Tried to wrap some event handling methods with anonymous functions to
    see if I get any results.

--- js/HomeView.js
+++ js/HomeView.js
@@ -3,7 +3,8 @@ var HomeView = function(store) {
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
-        this.el.on('keyup', '.search-key', this.findByName);
+        var that = this; 
+		this.el.on('keyup', '.search-key', function(){that.findByName();});
     };
 
     this.render = function() {
